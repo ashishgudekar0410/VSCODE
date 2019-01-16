80cffed84e70ce0faa418c22ea73b59799525972
Ashish Gudekar <ashish_gudekar@persistent.co.in>
2019-01-16 09:52:26 +0530

    Updated Message from Toast .

--- force-app/main/default/aura/ToastDemo/ToastDemoHelper.js
+++ force-app/main/default/aura/ToastDemo/ToastDemoHelper.js
@@ -3,7 +3,7 @@
     var toastEvent = $A.get("e.force:showToast");
     toastEvent.setParams({
         "title": "Success!",
-        "message": "The record has been updated successfully."
+        "message": "The record has been updated successfully.Test2"
     });
     toastEvent.fire();
 }
